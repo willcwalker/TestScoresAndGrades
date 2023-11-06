3076ac3d081e3abc7e674433729b7efd0ef0abd9
William Walker <william.c.walker.1@gmail.com>
2023-11-06 01:23:37 -0500

    reformmated generate_grade()

--- HwModelMunozEtAl.py
+++ HwModelMunozEtAl.py
@@ -4,30 +4,28 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib import font_manager
 
-
-
-
 plt.style.use('ggplot')
 rcParams['font.family'] = 'EB Garamond'
 rcParams['font.size'] = 14  # You can adjust the global font size as needed
 
-
 # Seed the random number generator for reproducibility
 np.random.seed(42)
+
 
 # Function to generate grades based on quadratic relationship with homework hours
 def generate_grade(hours, a, b, c, noise_level):
     # Calculate the grades based on the quadratic equation
-    grades = a * hours**2 + b * hours + c
-    
+    grades = a * hours ** 2 + b * hours + c
+
     # Adding some random Gaussian noise to each grade
     noise = np.random.normal(0, noise_level, len(hours))
     grades += noise
-    
+
     # Ensure grades are within the 0 - 100 range after adding noise
     grades = np.clip(grades, 0, 100)
-    
+
     return grades
+
 
 # Number of students
 num_students = 1500
@@ -37,8 +35,7 @@ hours_spent = np.random.uniform(0, 24, num_students)
 
 # Coefficients for the quadratic equation
 h = 10.5  # Assuming maximum grade is achieved at 12 hours of study.
-k = 100 # Assuming the maximum grade is 100.
-
+k = 100  # Assuming the maximum grade is 100.
 
 a = -0.15  # Adjust this value as needed to control the curvature
 
@@ -57,10 +54,8 @@ students_data = pd.DataFrame({
     'Grade': grades
 })
 
-
 # Display the first few rows of the DataFrame
 print(students_data.head())
-
 
 # Assuming 'x' is your independent variable and 'y' is your dependent variable
 # Let's say you have a pandas DataFrame 'df' with 'HoursHomework' as x and 'Grade' as y
@@ -81,19 +76,17 @@ y_poly = polynomial(x_poly)
 # Calculate R squared
 y_hat = polynomial(x)  # Your regression model as a function of x
 y_bar = np.mean(y)  # Mean value of y
-ss_reg = np.sum((y_hat - y_bar)**2)  # Sum of squares of the model
-ss_tot = np.sum((y - y_bar)**2)  # Total sum of squares
+ss_reg = np.sum((y_hat - y_bar) ** 2)  # Sum of squares of the model
+ss_tot = np.sum((y - y_bar) ** 2)  # Total sum of squares
 r_squared = ss_reg / ss_tot  # Calculation of R^2
-
-
 
 # Create the scatter plot with a larger figure size
 plt.figure(figsize=(10, 6))  # You can adjust the figure size to your preference
 
 # Scatter plot with smaller dots
-plt.scatter(x, y, s=10, color='#003781') #
+plt.scatter(x, y, s=10, color='#003781')  #
 
-#remove gridlines
+# remove gridlines
 plt.grid(False)
 
 # Add the polynomial line to the plot
@@ -114,10 +107,10 @@ plt.ylabel('Predicted Exam Grade', fontsize=14)
 
 # Move the legend outside the plot
 plt.legend(loc='upper right', bbox_to_anchor=(1, -.15), shadow=False, ncol=2)
-#plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
 
 # Add a grid
-#plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 
 # Improve the tick marks
 plt.xticks(fontsize=12)
@@ -127,10 +120,5 @@ plt.yticks(fontsize=12)
 plt.tight_layout()  # Adjusts the plot to ensure everything fits without overlapping
 plt.show()
 
-
-
-
 # Save to a CSV file (optional)
 students_data.to_excel('/Users/william/Desktop/Test Data/studentGradesData.xlsx', index=False)
-
-
